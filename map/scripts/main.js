75aa08d34ec84d479dbfe5dc1d2c1657a1d3b229
warjwarj <89038084+warjwarj@users.noreply.github.com>
2022-10-23 21:40:52 +0100

    start on menu - fix bug not loading all vehicles

--- map/scripts/main.js
+++ map/scripts/main.js
@@ -1,3 +1,9 @@
+/*
+
+
+
+*/
+
 
 const map = L.map('map').setView([51.505, -0.09], 7);
 
@@ -6,80 +12,40 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+let markers = []
 
-
-L.FeatureGroup.include({
-
-  // treu if vehicle is marked on the map
-  vehicleMarked: function(reg){
-    let x = false
-    this.eachLayer(function(layer){
-      if (layer.options.vName === reg){
-        x = true;
-    }})
-    return x
-  },
-
-  // custom getLayer searhing on vehicle name
-  getVehicle: function(vName){
-    this.eachLayer(function(layer){
-      if (layer.options.vName === vName){
-        return layer
-      }
-    })
-  }
-
-})
-
-markers = new L.FeatureGroup;
 
 function handleMarker(obj){
-  if (!markers.vehicleMarked(obj.registration)){
-    L.marker([obj.latitude, obj.longitude], {
+  // if marker not there create it
+  if (!(obj.id in markers)){
+    markers[obj.id] = L.marker([+(obj.latitude), +(obj.longitude)], {
       vName: obj.registration,
       vData: obj
     })
     .bindPopup(`Reg: ${obj.registration}`)
-    .addTo(markers)
-    console.log("new")
+    .addTo(map)
   } else {
-    markers.getVehicle(obj.registration).setLatLng([obj.latitude, obj.longitude])
-    console.log("updated")
+    // if marker is there update its position
+    markers[obj.id].setLatLng([+(obj.latitude), +(obj.longitude)])
   }
 }
 
-async function callApi(){
-    let request = await fetch('http://127.0.0.1:3000/proxy/apirouter', { 
-    method: 'GET',  
-    rejectUnauthorized: false,
-  })
-  let data = await request.json()
-  console.log(data)
-  return data
-}
 
 
 
 // handle the data returned by the api call
-// assume data is an array of objects
 async function refresh(newdata){
-  try {
-    markers.clearLayers()
-  } finally {
-    newdata.forEach(obj => {
-      handleMarker(obj)
-    });
-    markers.addTo(map)
-  }
+  newdata.forEach(obj => {
+    handleMarker(obj)
+  });
 }
 
-async function callApi(){
-  let request = await fetch('http://127.0.0.1:3000/proxy/apirouter', { 
+async function callApi(firstLoad){
+  let request = await fetch('http://127.0.0.1:3000/proxy/apirouter?firstLoad=' + firstLoad, { 
   method: 'GET',  
   rejectUnauthorized: false,
 })
 let data = await request.json()
-console.log(data)
 return data
 }
 
@@ -88,16 +54,18 @@ async function timerFunc(){
   function sleep(ms){
     return new Promise(resolve => setTimeout(resolve, ms))
   }
+  let first = true
   while (true){
-    callApi().then(
+    callApi(first).then(
       data => refresh(data)
     )
     console.log("refreshed")
+    first = false
     await sleep(60000)
   }
 }
 
-timerFunc()
+// timerFunc()
 
 
 
--- routes/proxy.js
+++ routes/proxy.js
@@ -5,11 +5,9 @@ const https = require('https')
 
 /*
 
-pass the latest, most recent datelastUpdated value to only get the vehicles which have updated since then.
+GET AN SSL CERT - rejectUnauthorized: false NOT GOOD
 
-need a cors proxy server for development?
-
-"https://api.easytrack.ltd/api/Sixfold/Vehicles?UpdatedAfter"
+"https://api.easytrack.ltd/api/Sixfold/Vehicles?UpdatedAfter="
 
 "LsVntsEp6XBab4CjF5ZAHLhL7YuWxBtdK2fqNwOIdx33H5tp4S0DNxA5sR6oQWFKPcIPH7KFzGY+/buopufYBXL+K+4mQUZKQK04/kgH3+c+oV/wXMOdodvwRIDBPk9B" - FW Fleet
 "wZA9rWWqD+wmCqABoEyVRKSUz9tvHycStxEOBh71qNOqRFBRFqstxMrC4wiqdkxQ83lmKxQW2SJ9GjqtG6Z4S7gfkp+3l7PFRdkjhIwzu18jRcmU1Lkcd8X/vvFlqb9Y" - GT
@@ -18,25 +16,28 @@ need a cors proxy server for development?
 
 let lastUpdatedAfter = ''
 
-function sortDates(data){
-  let arr = [];
+async function getData(firstLoad){
 
-}
 
-async function getData(){
     const httpsAgent = new https.Agent({
         rejectUnauthorized: false
     })
-    const url = 'https://api.easytrack.ltd/api/Sixfold/Vehicles?UpdatedAfter=' + lastUpdatedAfter
-    console.log(url)
-    const response = await fetch(url, {
+
+    function amountOfV(){
+        if (firstLoad == "true"){
+            return '';
+        } else {
+            return lastUpdatedAfter
+        }
+    }
+
+    const response = await fetch('https://api.easytrack.ltd/api/Sixfold/Vehicles?UpdatedAfter=' + amountOfV(), {
         method: 'GET',
         headers: {
             'Authorization': 'bearer wZA9rWWqD+wmCqABoEyVRKSUz9tvHycStxEOBh71qNOqRFBRFqstxMrC4wiqdkxQ83lmKxQW2SJ9GjqtG6Z4S7gfkp+3l7PFRdkjhIwzu18jRcmU1Lkcd8X/vvFlqb9Y',
         },
         agent: httpsAgent
     })
-    console.log(response)
     const data = await response.json()
     // get the latest date to pass back to api
     lastUpdatedAfter = data.map(function(e) { return e.dateLastUpdated; }).sort().reverse()[0]
@@ -45,9 +46,10 @@ async function getData(){
 
 
 router.get('/apirouter', async (req, res) => {
-    let data = await getData()
-    console.log(data.length)
+    let data = await getData(req.query.firstLoad)
+    console.log("returned " + data.length + " vehicles")
     res.send(data)
 })
+
 
 module.exports = router
