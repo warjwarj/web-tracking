4f723364757855fc72245fdea5817a193dbe0ee6
warjwarj <89038084+warjwarj@users.noreply.github.com>
2023-01-11 17:15:39 +0000

    pass error message into rendered page

--- middleware/checkPermissions.js
+++ middleware/checkPermissions.js
@@ -1,9 +1,9 @@
-const checkPermLvl = (requestedResourcePermLvl) => {
+const checkPermLvl = (requestedResourcePermLvl, error) => {
     return async (req, res, next) => {
         const user = await req.user
         if (user.permLevel < requestedResourcePermLvl){
             res.status(403).render('error.ejs', {
-                reason: "you do not have a high enough permission level to view the requested page. Please contact support if this is unexpected",
+                reason: error,
                 user: await req.user
             })
         } else {
