bbcd6950880fb2ec7ce40563afc66dd3b5cc686f
warjwarj <89038084+warjwarj@users.noreply.github.com>
2023-01-27 16:54:57 +0000

    assets changed

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,122 +1,66 @@
-// const pl = document.getElementById("pl").dataset.val
+function getChartConfig (){ return {  type: null, data: {}, options: {} } }
 
-// async function requestPerformanceData(queryParams=''){
-//     function customSort(data, key2SortOn){
-//         function customCompare(a, b){
-//           if (a[key2SortOn] < b[key2SortOn]){
-//             return -1;
-//           } else if (a[key2SortOn] > b[key2SortOn]){
-//             return 1;
-//           } else if (a[key2SortOn] == b[key2SortOn]){
-//             return 0;
-//           }
-//         }
-//         return data.sort(customCompare)
-//     }
-//     function formatParams(params){
-//       if (!params){ return ''; }
-//       let paramstring = ''
-//       for (let [key, val] of Object.entries(params)){ 
-//         key = '?' + key
-//         val = '=' + val
-//         paramstring += key + val;
-//       }
-//       return paramstring
-//     }
-//     let request = await fetch('/stats/data' + formatParams(queryParams), {
-//       method: 'GET',
-//       rejectUnauthorized: false,
-//       credentials: "include"
-//     })
-//     let driverdata = await request.json()
-//     if (driverdata.length){ driverdata = customSort(driverdata, "driverScore"); }
-//     return driverdata
-// }
+function getChartData (){ return { datasets: [], labels: [] } }
 
-// function minMaxScore(fleetdata){
-//     function getMinMax(arr, key) {
-//         const sortedArr = arr.sort((a, b) => b[key] - a[key]);
-//         const maxValues = sortedArr.slice(0, 3);
-//         const minValues = sortedArr.slice(-3).reverse();
-//         return { maxValues, minValues };
-//     }
-//     min = getMinMax(fleetdata)
-//     return (
-//         <div>
-//             <div className="main-block">
-//                 <h5>Highest three scores in fleet:</h5>
-//                 { max3.map(score => {
-//                     return <strong key={score}>{score}</strong>
-//                 }) }
-//             </div>
-//             <div className="main-block">
-//             <h5>Highest three scores in fleet:</h5>
-//             { min3.map(score => {
-//                 return <strong key={score}>{score}</strong>
-//             }) }
-//             </div>
-//         </div>
-//     )
-// }
+function getChartDataset (){ return { label: '', fill: true, data: [] } }
 
-// async function mainStats(scope){
-//     const homedata = await requestPerformanceData({ 'homedata': true,  scope })
-//     if (scope == 'driver'){
-//         return <p>{scope.driver}</p>
-//     }
-//     else if (scope == 'fleet'){
-//         return (
-//             minMaxScore()
-//         )
-//     }
-// }
-
-
-// const stats_root = ReactDOM.createRoot(document.getElementById("stats_root"))
-
-// if (pl == 1){
-//     stats_root.render(mainStats({ fleet: 'General Traffic' }));
-// }
-// else if (pl > 1){
-//     stats_root.render(mainStats({ driver: 'Stephen Dublin' }))
-// }
-/* <AsyncText url="/stats/data" parent='div' tag='p' content='Loading'/>, document.getElementById("stats_root") */
 
 const charts = [];
+const userdata = document.getElementById("userdata")
+const scopeData = JSON.parse(userdata.dataset.scopedata);
+const scope = userdata.dataset.scope;
+const pl = userdata.dataset.pl;
 
-function formatConfigDriver(chartType, driverData, keys){
-    const cfg = getChartConfig();
-    const dataobj = getChartData();
-    const possiblekeys = Object.keys(driverData[0]).filter(val => val != 'driver' && val != 'totalFuelUsed' && val != 'driverScore' && val != 'distance')
-    // charts that have multiple 'axis', multiple data points, radar, polar, etc.
-    if (keys == 'all'){
-      const datasets = []
-      for (let i in driverData){ // each driver obj - create a dataset for each
-        const dataset = getChartDataset()
-        dataset.label = driverData[i].driver
-        for (let j of possiblekeys){ // loop through keys
-          dataset.data.push(driverData[i][j])
-        }
-        datasets.push(dataset);
-        dataobj.datasets = datasets;
-        dataobj.labels = possiblekeys;
-        cfg.type = chartType;
-        cfg.data = dataobj;
-        return cfg
+
+function formatConfig(chartType, scopeData, keys, label){
+  const cfg = getChartConfig();
+  const dataobj = getChartData()
+  // charts that have multiple 'axis', multiple data points, radar, polar, etc.
+  if (keys == 'all'){
+    const possiblekeys = Object.keys(scopeData[0]).filter(val => val != 'driver' && val != 'totalFuelUsed' && val != 'driverScore' && val != 'distance')
+    const datasets = [];
+    for (let i in scopeData){ // each driver obj - create a dataset for each
+      const dataset = getChartDataset()
+      dataset.label = scopeData[i].driver
+      for (let j of possiblekeys){ // loop through keys
+        dataset.data.push(scopeData[i][j])
       }
-    } else {
-  
-      const xkey = keys[0]
-      const ykey = keys[1]
+      datasets.push(dataset);
+      dataobj.datasets = datasets;
+      dataobj.labels = possiblekeys;
+      cfg.type = chartType;
+      cfg.data = dataobj;
+      return cfg
     }
+  } else {
+    const xkey = keys[0]
+    const ykey = keys[1]
+    const datasets = [];
+    for (let i of scopeData){
+      // NEED TO MAKE SURE DATA IS SORTED
+    }
+  }
 }
 
 async function drawCharts(driver, fleet){
-    if (driver){
-      charts.forEach(chart => chart.destroy()) // clear charts on page
-      const data = []; data.push(driver) // get and add driver object/s to an array
-      const cfg = formatConfigDriver('polarArea', data, 'all'); // make config with driver/s data
-      let driverSummary = new Chart(document.getElementById("driver-events-summary"), cfg)
-      charts.push(driverSummary)
-    }
+  if (driver){
+    charts.forEach(chart => chart.destroy()) // clear charts on page
+    const data = []; data.push(driver) // get and add driver object/s to an array
+    const cfg = formatConfig('polarArea', data, 'all'); // make config with driver/s data
+    let driverSummary = new Chart(document.getElementById("driver-events-summary"), cfg)
+    charts.push(driverSummary)
+  }
+  else if (fleet){
+    charts.forEach(chart => chart.destroy()) // clear charts on page
+    const cfg = formatConfig('bar', fleet, ['driver', 'driverScore']); // make config with driver/s data
+    let fleet = new Chart(document.getElementById("fleet-score-summary"), cfg)
+    charts.push(fleetSummary)
+  }
+}
+
+if (scope == 'driver'){
+  drawCharts(scopeData, false)
+}
+else if (scope == 'fleet'){
+  drawCharts(false, scopeData)
 }
